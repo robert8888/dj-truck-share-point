765ce811793f524090d489f13ef7346aa1f520a3
unknown <robert.kami88@gmail.com>
2020-06-13 17:59:46 +0200

    Next opeh graph mix settings

--- routes/share/fb/view/index.js
+++ routes/share/fb/view/index.js
@@ -8,11 +8,12 @@ const mainPage = process.env.MAIN_PAGE;
 module.exports = (data) => ({
     fbAdminId,
     fbAppId,
-    currentUrl :  data.req.protocol + '://' + data.req.get('host') + data.req.originalUrl,
+    siteName: "Dj Truck",
+    currentUrl: data.req.protocol + '://' + data.req.get('host') + data.req.originalUrl,
     recordUrl: recordUrl + "/" + data.record.id,
-    pageUrl : mainPage + `/record/${data.record.user.nickname}/${data.record.title.replace(/\s/g, "_")}/${data.record.id}`,
-    imageUrl : data.record.user.picture,
-    recordDescription : data.record.description || data.record.title || "dj truck",
-    recordTitle : data.record.title,
+    pageUrl: mainPage + `/record/${data.record.user.nickname}/${data.record.title.replace(/\s/g, "_")}/${data.record.id}`,
+    imageUrl: data.record.user.picture,
+    recordDescription: data.record.description || data.record.title || "dj truck",
+    recordTitle: data.record.user.nickname + " - " + data.record.title,
     JS_GLOBAL_REDIRECT_URL: mainPage + `/record/${data.record.user.nickname}/${data.record.title.replace(/\s/g, "_")}/${data.record.id}`,
 })
