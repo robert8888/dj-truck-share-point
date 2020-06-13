157e59a2c20ad2dbbf6e85f1d4287d4854fe5b84
unknown <robert.kami88@gmail.com>
2020-06-13 16:41:14 +0200

    Add facebook domain verfication static files in static folder and epxress static files serving

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
-const express = require("express")
+const express = require("express");
+const path = require("path")
 const cors = require("cors")
 
 const PORT = process.env.PORT;
@@ -7,9 +8,16 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(cors());
 
+//static 
+app.use("/", express.static(path.join(__dirname, 'static')))
+
+
 //routes
 app.use(require("./routes/share"));
 app.use(require("./routes/404"))
+
+
+
 
 
 app.listen(PORT || 9000, () => {
